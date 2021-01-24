01e146cf9c4fe9e4c8252ba97402db13ff3986ba
metsimsek <metsimsek@gmail.com>
2021-01-24 14:14:06 +0000

    Home, Header, Footer components are added. Routing between components configured

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
   
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Con Fusion Restaurant</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
-
 export default App;
